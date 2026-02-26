0206f37133ee3a5ee9c55f147d177978e1101640
Amir Rehman Marwat <amirrehmanmwt@gmail.com>
2026-02-25 18:03:10 -0800

    learned that how do we connect our react with our backend server

--- chap24-react/src/App.jsx
+++ chap24-react/src/App.jsx
@@ -1,46 +1,65 @@
-import React from 'react'
-import { useForm } from "react-hook-form";
-
+import React, { useState } from 'react'
+import { useForm } from "react-hook-form"
+import axios from "axios"
 function App() {
-const {
+  // useStates
+  const [apiError, setapiError] = useState(null)
+  // React form hook methods:
+  const {
     register,
     handleSubmit,
     watch,
     setError,
-    formState: { errors,isSubmitting },
+    reset,
+    formState: { errors ,isSubmitting},
   } = useForm()
-  // function for submit :
-  const onSubmit=async()=>{
+
+  // Submit handler
+  const onSubmit=async(vals)=>{
+
     try {
-      const response=await fetch('https://jsonplaceholder.typicode.com/todos/1');
-    const data=await response.json();
-    console.log(data)
+      const response=await axios.post("http://localhost:3000/",{
+        method:"POST",
+        headers:{
+           'Content-Type': 'application/json',
+        },
+        body:JSON.stringify(vals)
+      })
+   
+      console.log("url fetch hogaya and data send hogaya")
+
+      console.log(response)
+      setapiError(undefined)
+      console.log(apiError)
     } catch (error) {
-      setError("serverError",{message:error.message})
-      
-      
-
+     setapiError(error.message)
+     reset()
     }
-
-  
+    
   }
   return (
     <div>
-{isSubmitting && <div>LOADING ...</div>}
-      <form className="form" onSubmit={handleSubmit(onSubmit)}>
-        <input type="text" defaultValue=""
-         {...register("username",{required:{value:true,message:"Name is required"},minLength:{value:5,message:"Your name must be of 5 letters"},maxLength:{value:10,message:"Your name must not exceed 10 letters"}})}  
-         placeholder='Enter you name'/>
+{isSubmitting && <div className='loading'>LOADING...</div>}
+     <form  className="form" onSubmit={handleSubmit(onSubmit)}> 
+
+        <input type="text" 
+          placeholder='Enter your name' 
+          defaultValue=""
+           {...register("username", { required: {value:true,message:"user name is required"}})}
+        
+        />
         {errors.username && <div className='error'>{errors.username.message}</div>}
         <br />
-        <input type="password" defaultValue=""
-        {...register("password",{required:{value:true,message:"a strong password is required"},minLength:{value:5,message:"Your password must not be less than 5 enteries"},maxLength:{value:10,message:"Your password should not exceed 10 enteries"}})}
-         placeholder='Enter you password' />
-         {errors.password && <div className='error'>{errors.password.message}</div>}
+        <input type="password" 
+          placeholder='Enter your name'
+          defaultValue=""
+          {...register("password", { required: {value:true,message:"user name is required"}})}
+          />
         <br />
-        <input disabled={isSubmitting}  type="submit" />
-      {errors.serverError && <div>{errors.serverError.message}</div>}
-      </form>
+           {errors.password && <div className='error'>{errors.password.message}</div>}
+        <input disabled={isSubmitting} type="submit" value="Submit" />
+        {apiError && <div className='apiError'>{apiError}</div>}
+  </form>
     </div>
   )
 }
